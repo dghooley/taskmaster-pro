4ba69e71309381cde00f35aff6eb15a6d32dc60b
Derick Hooley <dghooley@gmail.com>
2021-09-02 21:27:50 -0500

    droppable

--- assets/js/script.js
+++ assets/js/script.js
@@ -179,20 +179,55 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("deactivate", this);
+    console.log("activate", this);
   },
   deactivate: function(event) {
     console.log("deactivate", this);
   },
   over: function(event) {
+    console.log("over", event.target);
+  },
+  out: function(event) {
     console.log("out", event.target);
   },
-
   update: function(event) {
-$(this).children().each(function() {
-  console.log($(this));
-})
+    // array to store the task data in
+    var tempArr = [];
+    
+    // loop over current set of children in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+       .attr("id")
+       .replace("list-", "");
+
+    // update array on task object and save
+    tasks[arrName] = tempArr;
+    saveTasks();  
+
+    console.log(tempArr);
   }
-})
+});
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group")
+});
 
 loadTasks()
